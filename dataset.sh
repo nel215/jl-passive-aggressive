b307ba077a10788664aec6923fa879f16f2e48a4
nel215 <otomo.yuhei@gmail.com>
2014-07-26 09:05:33 +0900

    update dataset.sh.

--- dataset.sh
+++ dataset.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv
+if ! [ -e ./winequality-red.csv ] ; then
+    wget https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv
+fi
+
+if ! [ -e ./rcv1_train.binary.bz2 ] ; then
+    wget www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_train.binary.bz2
+fi
+
+if ! [ -e ./rcv1_train.binary ] ; then
+    bzip2 -d ./rcv1_train.binary.bz2
+fi
